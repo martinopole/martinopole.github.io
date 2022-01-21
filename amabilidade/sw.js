e2676cea9821d2a518c1cbcf1d733c5134b4392c
Milton Rodrigues <digita883@gmail.com>
2022-01-21 13:03:33 -0300

    correção pwa

--- amabilidade/sw.js
+++ amabilidade/sw.js
@@ -1,15 +1,38 @@
-// sw.js
-self.addEventListener('install', e => {
-	e.waitUntil(
-	// depois que o Service Worker estiver instalado,,
-	// abra um novo cache
-		caches.open('my-pwa-cache').then(cache => {
-	// adicione todas as URLs de recursos que queremos armazenar em cache
-			return cache.addAll([
-			'/',
-			'/index.html',
-			'/img/icone.jpg',
-			]);
-		})
-	);
+var CACHE_NAME = 'static-v1';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/img/icone.png',
+        '/sw.js',
+        '/manifest.js',
+      ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
