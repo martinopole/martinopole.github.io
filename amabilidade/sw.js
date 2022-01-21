67f4b24097b3ba82f987dcf5c25e37ca5bd73eef
Milton Rodrigues <digita883@gmail.com>
2022-01-21 18:11:58 -0300

    teste pwa

--- amabilidade/sw.js
+++ amabilidade/sw.js
@@ -13,27 +13,3 @@ self.addEventListener('install', function (event) {
     })
   )
 });
-/*
-self.addEventListener('activate', function activator(event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys
-        .filter(function (key) {
-          return key.indexOf(CACHE_NAME) !== 0;
-        })
-        .map(function (key) {
-          return caches.delete(key);
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
-*/
